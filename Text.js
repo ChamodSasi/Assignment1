2358dc54ea6bd8d52a786176f86bd4a466148931
ChamodSasi <chamodsasitha19@gmail.com>
2023-12-11 23:17:28 +0530

    inital files 2

---
+++ Text.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const content = document.getElementById("content");
+
+    // Function to execute document commands
+    function formatDoc(command, value = null) {
+        document.execCommand(command, false, value);
+    }
+
+
+
+
+
+
+
+
+
+
+
+    
+});
